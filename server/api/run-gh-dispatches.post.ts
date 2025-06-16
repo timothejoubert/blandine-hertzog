8c45e98c977da54af5bfa32182e499ccb697d690
timothe joubert <timothejoubert26@gmail.com>
2025-06-16 21:16:53 +0200

    build: add api route for gh dispatches

---
+++ server/api/run-gh-dispatches.post.ts
@@ -0,0 +1,24 @@
+export default defineEventHandler((event) => {
+    const runtimeConfig = useRuntimeConfig(event)
+
+    if (!runtimeConfig.github.userToken) {
+      return setResponseStatus(event, 503, 'Missing userToken to run github dispatch event')
+    }
+    
+    const githubDispatchUrl = `${runtimeConfig.github.repo}/dispatches`
+
+    return $fetch(githubDispatchUrl, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `token ${runtimeConfig.github.userToken}`,
+        },
+        body: {
+            event_type: 'prismic_publish_trigger',
+            client_payload: {
+                unit: false,
+                integration: true,
+            },
+        },
+    })
+})
