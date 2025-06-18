7d898857771e6f3d5b91759d8b1d0863eed3d727
timothe joubert <timothejoubert26@gmail.com>
2025-06-19 00:31:46 +0200

    refactor: clean api route for webhook

--- server/api/run-gh-dispatches.post.ts
+++ server/api/run-gh-dispatches.post.ts
@@ -1,26 +1,42 @@
 export default defineEventHandler(async (event) => {
     const runtimeConfig = useRuntimeConfig(event)
 
-    if (!runtimeConfig.github.userToken) {
-      return setResponseStatus(event, 403, 'Missing userToken to run github dispatch event')
+    const githubUserToken = runtimeConfig.github.userToken
+    if (!githubUserToken) {
+        return new Response('Missing userToken to run github dispatch event', { status: 403 })
     }
 
-    await $fetch('https://api.github.com/repos/timothejoubert/blandine-hertzog/dispatches', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/vnd.github+json',
-            Authorization: `token ${runtimeConfig.github.userToken}`,
-        },
-        body: {
-            event_type: 'preprod_api_publish_trigger',
-            client_payload: {
-                unit: false,
-                integration: true,
-            },
-        },
-    })
+    const prismicWebhookPsw = runtimeConfig.prismic.webhookPassword
+    const body = await readBody(event)
 
-    return new Response(`github dispatch event successfully send with 'preprod_api_publish_trigger' event_type`, { 
-        status: 200,
-    }); 
+    if (prismicWebhookPsw && body?.secret !== prismicWebhookPsw) {
+        return new Response("Invalid webhook secret.", { status: 403 });
+    }
+
+    try {
+        await $fetch('https://api.github.com/repos/timothejoubert/blandine-hertzog/dispatches', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/vnd.github+json',
+                Authorization: `token ${githubUserToken}`,
+                'User-Agent': 'blandine-hertzog/1.0',
+            },
+            body: {
+                event_type: 'preprod_api_publish_trigger',
+                client_payload: {
+                    unit: false,
+                    integration: true,
+                },
+            },
+        })
+
+        return new Response(
+            `GitHub dispatch event successfully sent with 'preprod_api_publish_trigger'`,
+            { status: 200 }
+        )
+
+    } catch (err) {
+        console.error('GitHub dispatch error:', err)
+        return new Response('Error triggering GitHub dispatch.', { status: 500 })
+    } 
 })
