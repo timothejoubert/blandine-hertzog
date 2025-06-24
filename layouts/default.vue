7efe9369edde0e6c5687f33b5ccbefdf6055fde8
timothe joubert <timothejoubert26@gmail.com>
2025-06-24 20:26:19 +0200

    a11y: add NuxtRouteAnnouncer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,6 +19,7 @@ const state = useSplashScreenState()
 
 <template>
     <template v-if="!isSliceSimulatorRoute">
+        <NuxtRouteAnnouncer />
         <ClientOnly>
             <VGridVisualizer />
             <VMediaViewer />
